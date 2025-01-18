95e36099ab89c68aab8219541f3bce9c3691a867
ashutosh_j <ashutosh.j001@gmail.com>
2025-01-19 00:04:41 +0530

    knapsack

--- programs/knapsack_with_repetation.cpp
+++ programs/knapsack_with_repetation.cpp
@@ -16,3 +16,22 @@ class Solution {
         return ans[n-1][capacity];
     }
 };
+
+// space optimized
+
+class Solution {
+  public:
+    int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
+        // code here
+        int n = val.size();
+        vector<int> ans(capacity+1,0);
+        if (wt[0] <= capacity) ans[wt[0]] = val[0];
+        for (int j = wt[0]+1 ; j <= capacity ; j++) ans[j] = ans[j-wt[0]]+val[0];
+        for (int i = 1 ; i < n ; i++){
+            for (int j = wt[i] ; j <= capacity ; j++){
+                ans[j] = max(ans[j] , ans[j-wt[i]]+val[i]);
+            }
+        }
+        return ans[capacity];
+    }
+};
