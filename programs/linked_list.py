09423f6a877110bbfe63209b4b1f9e0920360e59
ashutosh_j <ashutosh.j001@gmail.com>
2025-02-01 17:03:15 +0530

    updates

--- programs/linked_list.py
+++ programs/linked_list.py
@@ -1,29 +1,50 @@
-class Linked_list:
-    def __init__(self,val,p=None):
+class Node:
+    def __init__(self,val,next = None):
         self.val = val
-        self.p = p
+        self.next = next
+
+class Linked_list:
+    def __init__(self):
+        self.head = None
 
     def add_node(self,val):
-        current = self
-        while(current.p):
-            current = current.p
-        current.p = Linked_list(val)
+        new_node = Node(val)
+        if (not self.head):
+            self.head = new_node
+            return
+        current = self.head
+        while(current.next):
+            current = current.next
+        current.next = new_node
     
     def print_ll(self):
-        current = self
-        while(current.p):
+        current = self.head
+        while(current):
             print(current.val,end = (" "))
-            current = current.p
-        print(current.val)
+            current = current.next
+        print()
 
-n1 = Linked_list(5)
-n1.add_node(10)
-n1.add_node(11)
-n1.add_node(12)
-n1.add_node(13)
-n1.add_node(14)
-n1.add_node(15)
-n1.add_node(16)
-n1.add_node(17)
-n1.add_node(18)
-n1.print_ll()
+    def search_ll(self,t):
+        if (not self.head):
+            return -1
+        current = self.head
+        while(current):
+            if (current.val == t):
+                return current
+            current = current.next
+        return -1
+
+l1 = Linked_list()
+l1.add_node(10)
+l1.add_node(11)
+l1.add_node(12)
+l1.add_node(13)
+l1.add_node(14)
+l1.add_node(15)
+l1.add_node(16)
+l1.add_node(17)
+l1.add_node(18)
+l1.print_ll()
+l2 = Linked_list()
+pos = l2.search_ll(8)
+print(pos)
--- programs/tree.py
+++ programs/tree.py
@@ -1,35 +1,50 @@
-class Tree:
-    def __init__(self,val,l= None,r = None):
+class Node:
+    def __init__(self,val,l=None,r=None,p=None):
         self.val = val
         self.l = l
         self.r = r
+        self.p = p
+
+class Tree:
+    def __init__(self):
+        self.root = None
         
     def add_right_child(self,x):
-        if not self.r:
-            self.r = Tree(x)
+        new_node = Node(x)
+        if not self.root:
+            self.root = new_node
             return
-        current = self.r
+        current = self.root
         while (current.r and current.l):
             current = current.r
         if (current.r):
-            current.l = Tree(x)
+            current.l = new_node
         else:
-            current.r = Tree(x)
+            current.r = new_node
 
     def add_left_child(self,x):
-        current = self
-        # while
-        self.l = Tree(x)
+        new_node = Node(x)
+        if not self.root:
+            self.root = new_node
+            return
+        current = self.root
+        while(current.l):
+            current = current.l
+        current.l = new_node
     
-    def pre_order(self):
-        print(self.val,end=" ")
-        if self.l:
-            self.l.pre_order()
-        if self.r:
-            self.r.pre_order()
+    def pre_order(self,n = None):
+        if not n:
+            n = self.root
+        if not n:
+            return
+        print(n.val,end=" ")
+        if n.l:
+            self.pre_order(n.l)
+        if n.r:
+            self.pre_order(n.r)
         
 
-n1 = Tree(2)
+n1 = Tree()
 n1.add_left_child(3)
 n1.add_right_child(4)
 n1.add_right_child(5)
