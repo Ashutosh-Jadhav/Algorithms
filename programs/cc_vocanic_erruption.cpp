becbdba0f1e15a35ee6e4fbcbbafa8055cf7c6b0
ashutosh_j <ashutosh.j001@gmail.com>
2025-02-19 00:34:18 +0530

    updates

--- programs/cc_vocanic_erruption.cpp
+++ programs/cc_vocanic_erruption.cpp
@@ -2,10 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> f(vector<int>& arr){
+vector<double> f(vector<double>& arr){
     int n = arr.size();
     stack<int> s;
-    vector<int> b(n,0);
+    vector<double> b(n,0);
     s.push(0);
     for (int i= 0 ; i < n ; i++){
         if (arr[i] == 0){
@@ -28,14 +28,15 @@ vector<int> f(vector<int>& arr){
 
 int main() {
 	// your code goes here
-	int t , n, p ;
+	double t , n, p ;
 	cin >> t ;
 	while(t--){
 	    cin >> n >> p ;
-	    vector<int> arr(n,0);
-	    vector<int> b1(n,0);
-	    vector<int> b2(n,0);
-	    vector<int> ans(n,0);
+	    vector<double> arr(n,0);
+	    vector<double> b1(n,0);
+	    vector<double> b2(n,0);
+	    vector<double> ans(n,0);
+	    int r_f,l_f,r_in;
 	    for (int i = 0 ; i < n ; i++) cin >> arr[i] ;
 	    b1 = f(arr);
 	    reverse(arr.begin(),arr.end());
@@ -49,9 +50,18 @@ int main() {
 	    reverse(arr.begin(),arr.end());
 	   // for (int i = 0 ; i < n ; i++) cout << b2[i] ;
 	   // cout << endl;
+	   r_f = 1 ; l_f = 0 ;
+	   for (int i = 0 ; i < n ; i++) if (arr[n-i-1] == 0) { r_in = n-i-1;break; }
 	   for (int i= 0 ; i < n ; i++) {
-	       ans[i] = min(ceil(arr[b1[i]]/p),ceil(arr[b2[i]]/p));
+	       if (i > r_in) r_f = 0 ;
+	       if (arr[i] == 0 && l_f == 0) l_f = 1 ;
+	       if (l_f == 1 && r_f == 1 && arr[i] != -1) ans[i] = min(ceil(arr[b1[i]]/p),ceil(arr[b2[i]]/p));
+	       if (l_f == 1 && r_f == 0 && arr[i] != -1) ans[i] = ceil(arr[b2[i]]/p);
+	       if (l_f == 0 && r_f == 1 && arr[i] != -1) ans[i] = ceil(arr[b1[i]]/p);
 	       //if (ans[i] < ceil(arr[i]/p)) ans[i] = ceil(arr[i]/p);
+	   }
+	   for (int i= 0 ; i < n ; i++) {
+	       if (arr[i] == 0) ans[i] = 0;
 	   }
 	   for (int i= 0 ; i < n ; i++) cout << ans[i] << " ";
 	   cout << endl;
