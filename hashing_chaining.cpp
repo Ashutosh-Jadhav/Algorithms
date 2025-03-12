921fdaa4880adb3476d9323c79cf5af07f0fd50f
ashutosh_j <ashutosh.j001@gmail.com>
2025-03-12 23:58:42 +0530

    updates

---
+++ hashing_chaining.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node {
+    public:
+        int val;
+        class node *next;
+
+    public:
+        void node(int val , class node *next){
+            next = NULL;
+        }
+}
+
+class linked_list {
+    public:
+        class node *head;
+
+    public:
+        void linked_list(class)
+    
+}
