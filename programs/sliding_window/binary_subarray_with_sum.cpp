18105cbf6ba606adb8433a27c5d2457eb9816286
ashutosh_j <ashutosh.j001@gmail.com>
2025-08-28 18:03:29 +0530

    updates

---
+++ programs/sliding_window/binary_subarray_with_sum.cpp
@@ -0,0 +1,41 @@
+// BruteForce                              TLE
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size(),sum = 0,c=0;
+        for (int k = 1 ; k <= n ; k++){
+            for (int i = 0 ; i <= n-k ; i++) {
+                sum = 0;
+                for (int j = i ; j < i+k ; j++) {
+                    sum += nums[j];
+                }
+                if (sum == goal) c++;
+            }
+        }
+        return c;
+    }
+};
+
+
+// using hashing
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int n = nums.size(),sum = 0,c=0;
+        vector<int>prefix(n,0);
+        unordered_map<int,int> um;
+        um.insert({0,1});
+        for (int i = 0 ; i < n ; i++) {
+            sum = sum + nums[i];
+            prefix[i] = sum;
+            if (um.find(-goal+prefix[i]) != um.end()) {
+                c += um[-goal+prefix[i]];
+            }
+            if (um.find(prefix[i]) == um.end()) um.insert({prefix[i],1});
+            else um[prefix[i]]++;
+        }
+        return c;
+    }
+};
